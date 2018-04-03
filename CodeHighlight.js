e77ccfb63b2d17a70e9f1995d81295a56719e49d
Alvinyw <alvin.yiwen@gmail.com>
2018-04-03 13:33:13 +0800

    s

---
+++ CodeHighlight.js
@@ -0,0 +1,48 @@
+$(function() {
+    var elementsName = new Array("html", "head", "title", "body", "div", "button", "a", "p", "span", "style", "script", "input", "link", "meta", "img", "br", "hr"),
+        elementsAttr = new Array("content", "id", "value", "alt", "title", "style", "type", "src", "href", "rel", "dir", "lang", "name", "onBlur", "onClick", "onclick", "onFocus", "onKeyUp", "onKeyDown", "onKeyPress");
+	
+	/*将 &amp; 转换成 &（因为有时 &lt; 和 &gt; 里的 & 会被误操作转义成 &amp;）*/
+    do {
+        $("#CodeHighlight").html($("#CodeHighlight").html().replace(/&amp;/g, "&"))
+    } while (-1 != $("#CodeHighlight").html().indexOf("&amp;"));
+	
+	/*将中文的双引号 “” 转换成英文的双引号 ""*/
+    do {
+        $("#CodeHighlight").html($("#CodeHighlight").html().replace("“", '"').replace("”", '"'))
+    } while (-1 != $("#CodeHighlight").html().indexOf("”"));
+	
+	/*将中文的单引号 ’‘ 转换成英文的单引号 '*/
+    do {
+        $("#CodeHighlight").html($("#CodeHighlight").html().replace("‘", "'").replace("’", "'"))
+    } while (-1 != $("#CodeHighlight").html().indexOf("’"));
+	
+	/*遍历数组 elementsName 为标签名加上 class = "keyword"*/
+    for (var e = 0; e < elementsName.length; e++){
+		
+        do {
+			/*将 &lt;div 替换成 &lt;<span class="keyword">div</span>*/
+            $("#CodeHighlight").html($("#CodeHighlight").html().replace("&lt;" + elementsName[e], '&lt;<span class="keyword">' + elementsName[e] + "</span>"));
+			
+			/*将 &lt;/div 替换成 <&lt;<span class="plain">/</span><span class="keyword">div</span>*/
+			$("#CodeHighlight").html($("#CodeHighlight").html().replace("&lt;/" + elementsName[e], '&lt;<span class="plain">/</span><span class="keyword">' + elementsName[e] + "</span>"))
+			
+        } while (-1 != $("#CodeHighlight").html().indexOf("&lt;" + elementsName[e]));
+		
+	}
+	
+	/*遍历数组 elementsAttr 为标签属性加上 class = "color1"*/
+    for (var e = 0; e < elementsAttr.length; e++){
+        
+		do {
+            $("#CodeHighlight").html($("#CodeHighlight").html().replace(elementsAttr[e] + '="', '<span class="color1">' + elementsAttr[e] + '</span><span class="plain">=</span><span class="string">"'));
+			
+			$("#CodeHighlight").html($("#CodeHighlight").html().replace('" ', '"</span> ')), $("#CodeHighlight").html($("#CodeHighlight").html().replace('"&gt;', '"</span>&gt;'));
+			
+        } while (-1 != $("#CodeHighlight").html().indexOf(elementsAttr[e] + '="'));
+	
+	}
+	
+    $("#CodeHighlight").html($("#CodeHighlight").html().replace(/&lt;/g, '<span class="plain">&lt;</span>').replace(/&gt;/g, '<span class="plain">&gt;</span>').replace(/&frasl;/g, '<span class="plain">&frasl;</span>'));
+	
+});
